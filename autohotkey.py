a3177b1adfe4bf1541fd65003d08357e72c7dee4
Fran <hola@beachlab.org>
2020-10-12 22:16:58 +0200

    update autokey

--- autohotkey.py
+++ autohotkey.py
@@ -37,7 +37,7 @@ cable = 'sudo ip link set cable0 up && sudo dhcpcd cable0'
 online='sudo wifi-menu'
 loopcam='sudo modprobe v4l2loopback video_nr=9'
 htop='uxterm -b 5 -e htop'
-yetimic='alsa_in -j "Yeti" -d hw:Microphone -q 1 2>&1 1> /dev/null &'
+yetimic=''   # 'alsa_in -j "Yeti" -d hw:Microphone -q 1 2>&1 1> /dev/null &'
 
 # program launcher
 def launch(program):
@@ -68,7 +68,7 @@ try:
             if key.keycode == 'KEY_SPACE':
                 subprocess.call('xdotool key Super_L+space'.split())
             #
-            # ESC-F1-F12    
+            # ROW 1 ESC-F1-F12    
             #
             if key.keycode == 'KEY_ESC':
                 subprocess.call('xdotool key Super_L+f'.split())
@@ -116,54 +116,174 @@ try:
                 subprocess.call('xdotool key Super_L+Shift_L+space'.split())
                 print('Toggle floating window')
             if key.keycode == 'KEY_F14':
-                subprocess.call('xdotool key Super_L+Alt_Lspace'.split())
+                subprocess.call('xdotool key Super_L+Alt_L+space'.split())
                 print('Toggle focus tiling / floating')
             if key.keycode == 'KEY_F15':
                 subprocess.call('xdotool key Super_L+Shift_L+x'.split())
-
+                print('System locked, type password to unlock')
             #
-            # 1 .. 0
+            # ROW 2 1..0
             #
+            if key.keycode == 'KEY_102ND':
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+backslash'.split())
+                print('Studio mode')
             if key.keycode == 'KEY_1':
-                subprocess.call('xdotool key Alt_L+Shift_L+1'.split())
-                #print('Switch to scene 1')
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+1'.split())
+                print('Switch to scene 1')
             if key.keycode == 'KEY_2':
-                subprocess.call('xdotool key Alt_L+Shift_L+2'.split())
-                #print('Switch to scene 2')
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+2'.split())
+                print('Switch to scene 2')
             if key.keycode == 'KEY_3':
-                subprocess.call('xdotool key Alt_L+Shift_L+3'.split())
-                #print('Switch to scene 3')
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+3'.split())
+                print('Switch to scene 3')
             if key.keycode == 'KEY_4':
-                subprocess.call('xdotool key Alt_L+Shift_L+4'.split())
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+4'.split())
                 print('Switch to scene 4')
             if key.keycode == 'KEY_5':
-                subprocess.call('xdotool key Alt_L+Shift_L+5'.split())
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+5'.split())
                 print('Switch to scene 5')
             if key.keycode == 'KEY_6':
-                subprocess.call('xdotool key Alt_L+Shift_L+6'.split())
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+6'.split())
                 print('Switch to scene 6')
             if key.keycode == 'KEY_7':
-                subprocess.call('xdotool key Alt_L+Shift_L+7'.split())
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+7'.split())
                 print('Switch to scene 7')
             if key.keycode == 'KEY_8':
-                subprocess.call('xdotool key Alt_L+Shift_L+8'.split())
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+8'.split())
                 print('Switch to scene 8')
             if key.keycode == 'KEY_9':
-                subprocess.call('xdotool key Alt_L+Shift_L+9'.split())
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+9'.split())
                 print('Switch to scene 9')
             if key.keycode == 'KEY_0':
-                subprocess.call('xdotool key Alt_L+Shift_L+0'.split())
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+0'.split())
                 print('Switch to scene 0')
+            if key.keycode == 'KEY_MINUS':
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+minus'.split())
+                print('Switch to scene logo')
+            if key.keycode == 'KEY_EQUAL':
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+equal'.split())
+                print('Start/Stop recording')
+            if key.keycode == 'KEY_BACKSPACE':
+                subprocess.call('xdotool key Control_L+Alt_L+Shift_L+BackS333pace'.split())
+                print('Start streaming')
+
             #
-            # QWERTY...
+            # ROW 3 QWERTY...
             #
             if key.keycode == 'KEY_Q':
                 #msg = mido.Message("note_on", note=36, velocity=100, time=10)
                 #msg.copy(channel=1)
                 pass
+            if key.keycode == 'KEY_W':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_E':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_R':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_T':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_Y':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_U':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_I':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_O':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_P':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+
+            #
+            # ROW 4 ASDF...
+            #
+            if key.keycode == 'KEY_A':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_S':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_D':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_F':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_G':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_H':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_J':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_K':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_L':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            #
+            # ROW 5 ZXCVB...
+            #
+            if key.keycode == 'KEY_Z'
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_X':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_C':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_V':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_B':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_N':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_M':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_COMMA': # check
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_DOT': # check
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+
             #
             # PAGE UP DOWN
             #
+            if key.keycode == 'KEY_FN':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_HOME':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_PAGEUP':
+                subprocess.call('xdotool key '.split())  # FIX
+                print('Mute Zoom Stream')
+            if key.keycode == 'KEY_DELETE':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_END':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_PAGEDOWN':
+                subprocess.call('xdotool key '.split()) # FIX
+                print('PTT')
 
             #
             # KEYPAD
@@ -176,17 +296,21 @@ try:
                 print('Split horizontally')
             if key.keycode == 'KEY_F18':
                 subprocess.call('xdotool key Super_L+q'.split())
+                print('Window closed')
             if key.keycode == 'KEY_F19':
                 launch('xkill')
+                print('Window killed')
             if key.keycode == 'KEY_NUMLOCK':
                 os.system(loopcam)
-                print('Loopcam device on /dev/video9')
+                print('Loopcam device(s) activated')
             if key.keycode == 'KEY_KPEQUAL':
                 os.system(cable)
+                print('Ethernet DHCP activated')
             if key.keycode == 'KEY_KPSLASH':
                 os.system(yetimic)
+                print('Yeti Microphone')
             if key.keycode == 'KEY_KPASTERISK':
-                os.system(htop) ## block keyboard
+                os.system(htop) ## blocks keyboard
             if key.keycode == 'KEY_KPDOT':
                 os.system(online)
             if key.keycode == 'KEY_KP9':
@@ -221,6 +345,15 @@ try:
             if key.keycode == 'KEY_KPMINUS':
                 subprocess.call('xdotool key Super_L+Return'.split())
                 print('Open Terminal window')
+            if key.keycode == 'KEY_END':
+                subprocess.call('xdotool key '.split())
+                print('Unassigned')
+            if key.keycode == 'KEY_KP0':
+                subprocess.call('xdotool key '.split()) # FIX
+                print('Unassigned')
+            if key.keycode == 'KEY_KPENTER':
+                subprocess.call('xdotool key '.split()) # FIX
+                print('Unassigned')
             #
             # ARROW KEYS
             #
